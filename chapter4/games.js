77d0178801ce4d17ad33dc47aeb26597bd734619
tiko <tiko.pb@gmail.com>
2021-06-05 19:52:44 +0700

    make all validation and fix oop process on class

--- chapter4/games.js
+++ chapter4/games.js
@@ -10,84 +10,87 @@ const guntingPlayer = document.getElementById("gunting1").onclick = guntingActio
 const imagePath = "img/"
 const winImagePath = "/player1.png"
 const loseImagePath = "/comWin.png"
-let resultImg = document.getElementById("result")
+var resultImg = document.getElementById("result")
 
 //main function 
-function batuAction(){ 
-    if(plyChoose!=null){
-        return alert("Reset The Game please press refresh!")
+
+class gameStart {
+    constructor(ply){
+        this.ply = ply;
     }
-    getRandomMathOfCom()
-    plyChoose = 0
-    SetColorBackgroundCom(comChoose)
-    SetColorBackgroundPly(plyChoose)
-    validationOfTheWinner(plyChoose,comChoose)
-}
-
-function kertasAction(){ 
-    if(plyChoose!=null){
-        return alert("Reset The Game please press refresh!")
-    }
-    getRandomMathOfCom()
-    plyChoose = 1
-    SetColorBackgroundCom(comChoose)
-    SetColorBackgroundPly(plyChoose)
-    validationOfTheWinner(plyChoose,comChoose)
-}
-
-function guntingAction(){ 
-    if(plyChoose!=null){
-        return alert("Reset The Game please press refresh!")
-    }
-    getRandomMathOfCom()
-    plyChoose = 2
-    SetColorBackgroundCom(comChoose)
-    SetColorBackgroundPly(plyChoose)
-    validationOfTheWinner(plyChoose,comChoose)
-}
-
-function getRandomMathOfCom(){ // generate what com player choose on random math
-    comChoose = Math.floor(Math.random() * 3)  ;  
-}
-
-function SetColorBackgroundCom(comVariabelId){
-    let computerChoose = comVariabelIdList[comVariabelId];
-    console.log(computerChoose+" com")
-    document.getElementById(computerChoose).style.background='#808080';
-}
-
-function SetColorBackgroundPly(comVariabelId){
-    let playerChoose = player1VariabelIdList[comVariabelId];
-    console.log(playerChoose+" ply")
-    document.getElementById(playerChoose).style.background='#808080';
-}
-
-function validationOfTheWinner(player, computer){
-    let ply = parseInt(player)
-    let com = parseInt(computer)
-    console.log(ply + "  || " + com )
-    if(ply == com){
-        resultImg.src = imagePath+"/draw.png"
-    }else if (ply == 0){
-        if(com==1){
-            resultImg.src = imagePath+loseImagePath
-        }else{
-            resultImg.src = imagePath+winImagePath
-        }
-    }
-    else if (ply == 1){
-        if(com==2){
-            resultImg.src = imagePath+loseImagePath
-        }else{
-            resultImg.src = imagePath+winImagePath
-        }
-    }
-    else if (ply == 2){
-        if(com==0){
-            resultImg.src = imagePath+loseImagePath
-        }else{
-            resultImg.src = imagePath+winImagePath
+    
+    play(){
+        if(plyChoose!=null){
+            return alert("To Play again press refresh button!")
+        }else {
+            plyChoose = this.ply
+            this.getRandomMathOfCom()
         }
     }
     
+    getRandomMathOfCom(){ // generate what com player choose on random math
+        comChoose = Math.floor(Math.random() * 3)  ;  
+        this.validationOfTheWinner()
+    }
+    
+    validationOfTheWinner(){
+        let ply = parseInt(this.ply)
+        let com = parseInt(comChoose)
+        console.log(ply + "  || " + com )
+        if(ply == com){
+            resultImg.src = imagePath+"/draw.png"
+        }else if (ply == 0){
+            if(com==1){
+                resultImg.src = imagePath+loseImagePath
+            }else{
+                resultImg.src = imagePath+winImagePath
+            }
+        }
+        else if (ply == 1){
+            if(com==2){
+                resultImg.src = imagePath+loseImagePath
+            }else{
+                resultImg.src = imagePath+winImagePath
+            }
+        }
+        else if (ply == 2){
+            if(com==0){
+                resultImg.src = imagePath+loseImagePath
+            }else{
+                resultImg.src = imagePath+winImagePath
+            }
+        } 
+        
+        this.SetColorBackgroundPly()
+        this.SetColorBackgroundCom()
+    }
+        
+    SetColorBackgroundCom(){
+        let computerChoose = comVariabelIdList[comChoose];
+        console.log(computerChoose+" com")
+        document.getElementById(computerChoose).style.background='#808080'
+    }
+
+    SetColorBackgroundPly(){
+        let playerChoose = player1VariabelIdList[this.ply];
+        console.log(playerChoose+" ply")
+        document.getElementById(playerChoose).style.background='#808080';
+    }
+    
+}
+
+
+function batuAction(){ 
+    let playGame = new gameStart(0)
+    playGame.play()
+}
+
+function kertasAction(){ 
+    let playGame = new gameStart(1)
+    playGame.play()
+}
+
+function guntingAction(){ 
+    let playGame = new gameStart(2)
+    playGame.play()
 }
