b0c33af75563a8bc0b2b7c43bb895baa93292d90
tiko <tiko.pb@gmail.com>
2021-09-27 19:37:15 +0700

    firebase

---
+++ chapter9/zoom/react-js-spa/chapter9/src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ1n4qAuOcKoDwjlzuBEjn3ts4-kGM7p4",
+  authDomain: "binar-tiko.firebaseapp.com",
+  projectId: "binar-tiko",
+  storageBucket: "binar-tiko.appspot.com",
+  messagingSenderId: "435911137845",
+  appId: "1:435911137845:web:309b79e13361dfc1a6af7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
